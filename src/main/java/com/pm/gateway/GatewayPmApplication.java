a8705c2dd28a31171ef1fb6b0a1b6086e8903110
golubkovS <omon03@gmail.com>
2021-08-16 15:31:24 +0500

    Initial commit

---
+++ src/main/java/com/pm/gateway/GatewayPmApplication.java
@@ -0,0 +1,13 @@
+package com.pm.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayPmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pm/gateway/GatewayPmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayPmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
